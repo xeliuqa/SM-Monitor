421da59c3c0929b6d9f20c6ea147b3c936b69594
Jonh <92469159+xeliuqa@users.noreply.github.com>
2024-01-29 07:19:34 +0000

    Update SM-Monitor.ps1

--- SM-Monitor.ps1
+++ SM-Monitor.ps1
@@ -465,7 +465,7 @@ function main {
         $currentDate = Get-Date -Format HH:mm:ss
         # Refresh
         Write-Host `n
-        Write-Host "Press UPDATE to refresh" -ForegroundColor DarkGray
+        Write-Host "Press SPACE to refresh" -ForegroundColor DarkGray
         Write-Host "Last refresh:  " -ForegroundColor Yellow -nonewline; Write-Host "$currentDate" -ForegroundColor Green 
             
         # Get original position of cursor
